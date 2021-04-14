3abc4695f5513c544755b88584f40f1eca904d3c
Sravya Somisetty <ssravya.1996@gmail.com>
2021-04-13 23:09:18 -0700

    managing state with Redux store

--- src/SubTask.js
+++ src/SubTask.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { createStore } from 'redux';
 
 class SubTask extends React.Component {
+    // SUBTASK_CHECKED = 'SUBTASK_CHECKED';
+    store = createStore(this.checkSubTaskReducer.bind(this));
+    unsubscribe = ()=> {};
+
     constructor(props) {
         super(props);
 
-        this.state = {
-            checked: false
-        };
     }
+
+    componentDidMount(){
+        this.unsubscribe = this.store.subscribe(()=>{this.forceUpdate()});
+    }
+
 
     render() {
         const { item } = this.props;
-        const { checked } = this.state;
-
+        const { checked } = this.store.getState();
+        
         return (<label htmlFor={item}>
             <input type="checkbox" onChange={this.handleChange.bind(this)} id={item} />
             {checked ? item + 'checked' : item}
@@ -20,11 +27,29 @@ class SubTask extends React.Component {
     }
 
     handleChange(event) {
-        this.setState({
-            checked: event.target.checked
-        })
-        this.props.notifyTask(this.props.item, event.target.checked);
+        const checked = event.target.checked;
+        this.store.dispatch(this.subtaskActionCreator({
+            checked: checked
+        }))
     }
+
+    subtaskActionCreator(subtask) {
+        return {
+            type: 'SUBTASK_CHECKED',
+            subtask
+        }
+    }
+
+    checkSubTaskReducer(state = {}, action) {
+        console.log("reducer invoked");
+        if (action.type === 'SUBTASK_CHECKED') {
+            return { checked: !state.checked };
+        }
+        else {
+            return state;
+        }
+    }
+
 }
 
 export default SubTask;
